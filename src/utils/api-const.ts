62a1958d61e4df9b0081be95937d9f6883c030b7
TruongThanh <thanh.nguyen.truong@vieon.vn>
2023-07-16 03:15:26 +0700

    #add vercel json to rewrite project use proxy

--- src/utils/api-const.ts
+++ src/utils/api-const.ts
@@ -1,11 +1,7 @@
-const VIE_GG_FONT_API_KEY = "AIzaSyD_PqVd_50RnHjHX3pmnQUbF-AodrmmgV0";
-const VITE_API_URL = "/webfonts/v1/webfonts";
 const FONT_META_API_URL = "/meta/metadata/fonts";
 const FONT_TESTER_DESCRIPTION_API_URL = "/tester/sampletext";
 
 export const API = {
-  FONTS: `${VITE_API_URL}?key=${VIE_GG_FONT_API_KEY}`,
   META_DATA: FONT_META_API_URL,
-  SEARCH_META_DATA: FONT_META_API_URL,
   TESTER_DESCRIPTION: FONT_TESTER_DESCRIPTION_API_URL,
 };
